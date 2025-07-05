78431703d390df485180b78830c5ddb0ef79f821
Alejandro <alejandrogaratea@gmail.com>
2025-07-05 13:48:45 +0200

    escalera infinita + coliders y reorder de escenas

--- scripts/escaleras.gd
+++ scripts/escaleras.gd
@@ -4,18 +4,19 @@ extends Node2D
 var background_height: float
 
 func _ready():
-	background_height = $Fondo1.texture.get_height()
+	background_height = $Fondo1.texture.get_height() * $Fondo1.scale.y
 
 func _process(delta):
-	$Fondo1.position.y += scroll_speed * delta
-	$Fondo2.position.y += scroll_speed * delta
-	$Fondo3.position.y += scroll_speed * delta
+	$Fondo1.position.y -= scroll_speed * delta
+	$Fondo2.position.y -= scroll_speed * delta
+	$Fondo3.position.y -= scroll_speed * delta
 
-	if $Fondo1.position.y >= background_height:
-		$Fondo1.position.y = $Fondo2.position.y - background_height
+	if $Fondo1.position.y <= - background_height:
+		$Fondo1.position.y = $Fondo3.position.y + background_height
 
-	if $Fondo2.position.y >= background_height:
-		$Fondo2.position.y = $Fondo3.position.y - background_height
 
-	if $Fondo3.position.y >= background_height:
-		$Fondo3.position.y = $Fondo1.position.y - background_height
+	if $Fondo2.position.y <= - background_height:
+		$Fondo2.position.y= $Fondo1.position.y + background_height
+
+	if $Fondo3.position.y <= - background_height:
+		$Fondo3.position.y = $Fondo2.position.y + background_height
--- scripts/jugador.gd
+++ scripts/jugador.gd
@@ -1,10 +1,7 @@
 extends CharacterBody2D
 
 @export var speed: float = 200.0
-@export var min_x := 0.0
-@export var max_x := 720.0
-@export var min_y := 0.0
-@export var max_y := 1280.0
+
 
 func _physics_process(delta):
 	var direction = Vector2.ZERO
@@ -21,7 +18,3 @@ func _physics_process(delta):
 	direction = direction.normalized()
 	velocity = direction * speed
 	move_and_slide()
-
-	# Limitar posición
-	global_position.x = clamp(global_position.x, min_x, max_x)
-	global_position.y = clamp(global_position.y, min_y, max_y)
