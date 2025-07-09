972a57d5df25d0e3ed869283b866bcdd9df59f92
Alejandro <alejandrogaratea@gmail.com>
2025-07-09 19:31:08 +0200

    Ranking online

--- scripts/game_over_menu.gd
+++ scripts/game_over_menu.gd
@@ -3,3 +3,8 @@ extends Control
 func _ready():
 	$ScoreTxt.text = tr("LBL_SCORE") + ": " + str(GlobalVars.final_score)
 	await  $TransitionScene.transition_finished
+
+
+func _on_submit_pressed() -> void:
+	if $LineEdit.text != "":
+		SilentWolf.Scores.save_score($LineEdit.text, GlobalVars.final_score)
--- scripts/globals/global_variables.gd
+++ scripts/globals/global_variables.gd
@@ -14,3 +14,13 @@ func desbloquear_vecino(vecino_nuevo : ObstacleResource):
 	var indice_vecino = lista_vecinos.find(vecino_nuevo)
 	if (indice_vecino > -1):
 		lista_vecinos[indice_vecino].desbloqueado = true
+
+func _ready():
+	SilentWolf.configure({
+		"api_key": "NBlyZMvWWBazaxpwTHc2A1mJjs0gOVDg8UAhmy7I",
+		"game_id": "stairwaytohall",
+		"log_level": 1
+	})
+	SilentWolf.configure_scores({
+		"open_scene_on_close": "res://scenes/MainPage.tscn"
+	})
