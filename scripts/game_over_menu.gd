4cb5a8b930bc212e1888393cafa34ed3084dcf0c
Bea <beatriz.lagoperez102@gmail.com>
2025-07-10 18:39:56 +0200

    Feedback al guardar la puntuación :)

--- scripts/game_over_menu.gd
+++ scripts/game_over_menu.gd
@@ -1,9 +1,16 @@
 extends Control
 
 func _ready():
+	SilentWolf.Scores.sw_save_score_complete.connect(score_saved)
 	$ScoreTxt.text = tr("LBL_SCORE") + ": " + str(GlobalVars.final_score)
 	await  $TransitionScene.transition_finished
 
+func score_saved(sw_result : Dictionary):
+	if (sw_result["success"] == true):
+		$Msg.text = tr("LBL_MSG_SUCCESS")
+	else:
+		$Msg.text = tr("LBL_MSG_FAIL")
+		$Submit.disabled = false
 
 func _on_submit_pressed() -> void:
 	if $LineEdit.text != "":
