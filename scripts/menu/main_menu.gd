564a407784302c329e8c0259d9c7dd0ac5ec7a3f
Bea <beatriz.lagoperez102@gmail.com>
2025-07-13 17:23:23 +0200

    Arreglado un temita de las luces al cambiar de pantalla

--- scripts/menu/main_menu.gd
+++ scripts/menu/main_menu.gd
@@ -6,7 +6,11 @@ const intro_scene_path : String = "res://scenes/menus/intro.tscn"
 const game_scene_path : String = "res://scenes/game_scene.tscn"
 
 func _ready():
-	
+	var navigation_button_list = get_tree().get_nodes_in_group("NavButton")
+	for object in navigation_button_list:
+		var nav_button : NavigationButton = object
+		nav_button.pressed.connect(turn_off_lights)
+			
 	$Play.mouse_entered.connect(AudioManager.play_boton_select)
 	$Play.button_down.connect(AudioManager.play_boton_down)
 	$Play.pressed.connect(AudioManager.play_boton_pressed)
@@ -24,13 +28,14 @@ func _ready():
 
 
 func _on_play_pressed():
-	$Background/Lamp1/PointLight2D.visible = false
-	$Background/Lamp2/PointLight2D.visible = false
 	if GlobalVars.is_first_game:
 		$TransitionManager.play_fadeout_animation(intro_scene_path)
 	else:
 		$TransitionManager.play_fadeout_animation(game_scene_path)
 
+func turn_off_lights():
+	$Background/Lamp1/PointLight2D.visible = false
+	$Background/Lamp2/PointLight2D.visible = false
 
 func _on_play_mouse_entered():
 	$Play.rotation_degrees = 5.0
