0c33b1ba23c4874a5d0cb701530378a29b3f44b2
Bea <beatriz.lagoperez102@gmail.com>
2025-07-13 18:26:34 +0200

    Aura para el dash

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -5,5 +5,7 @@ func next_level(new_level : int):
 	$UiAnimations.play("next_level")
 
 func set_dash_visibility(b_enabled : bool):
-	var alfa = 1 if b_enabled else 0.5
-	$UIDash.modulate = Color(1, 1, 1, alfa)
+	var alfa_icono = 1 if b_enabled else 0.5
+	var alfa_aura = 1 if b_enabled else 0
+	$TextureRect.modulate = Color(1, 1, 1, alfa_aura)
+	$UIDash.modulate = Color(1, 1, 1, alfa_icono)
